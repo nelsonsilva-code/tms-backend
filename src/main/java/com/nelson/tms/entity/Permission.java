69f657890c6348eaedd378bfa2bb19b716e42993
nelsonsilva-code <nelson.silva@vwds.pt>
2025-06-11 17:11:10 +0100

    :sparkles: Add note to self :)

--- src/main/java/com/nelson/tms/entity/Permission.java
+++ src/main/java/com/nelson/tms/entity/Permission.java
@@ -1,5 +1,8 @@
 package com.nelson.tms.entity;
 
+
+//This is currently not in use. It will be used in the future when Role creation is configured
+//TODO: Create API to create customized roles
 public enum Permission {
     CREATE,
     READ,
