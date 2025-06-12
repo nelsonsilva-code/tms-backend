47ec83efd1ce0db502f243d6e15251c301cb29fc
nelsonsilva-code <nelson.silva@vwds.pt>
2025-06-12 11:30:44 +0100

    :sparkles: Add username validation

--- src/main/java/com/nelson/tms/dto/CreateUserDto.java
+++ src/main/java/com/nelson/tms/dto/CreateUserDto.java
@@ -12,6 +12,10 @@ public class CreateUserDto {
 
     private Long id;
     private String name;
+    @Pattern(
+            regexp = "^[a-z0-9_]{1,12}$",
+            message = "Username must be at most 12 characters long, contain only lowercase letters, no spaces and only allowed special characters: _"
+    )
     private String username;
     @Pattern(
             regexp = "^(?=.*[A-Z])(?=.{12,}$)(?!.*\\s)[A-Za-z\\d!@_-]+$",
