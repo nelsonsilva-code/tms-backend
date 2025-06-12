d4ea03cdde6c75f0a17cadece7a15ba6b7b4cf36
nelsonsilva-code <nelson.silva@vwds.pt>
2025-06-12 23:05:33 +0100

    :sparkles: Ignore password field in response body

--- src/main/java/com/nelson/tms/entity/User.java
+++ src/main/java/com/nelson/tms/entity/User.java
@@ -1,5 +1,6 @@
 package com.nelson.tms.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -21,6 +22,7 @@ public class User {
     @Column(nullable = false)
     private String username;
 
+    @JsonIgnore
     @Column(nullable = false)
     private String password;
 
