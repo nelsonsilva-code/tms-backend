c58d72286ef6de50c56f897af19781810a56e56b
nelsonsilva-code <nelson.silva@vwds.pt>
2025-06-12 11:41:37 +0100

    :sparkles: Add first name validation

--- src/main/java/com/nelson/tms/dto/CreateUserDto.java
+++ src/main/java/com/nelson/tms/dto/CreateUserDto.java
@@ -11,17 +11,24 @@ import lombok.*;
 public class CreateUserDto {
 
     private Long id;
+
+    @Pattern(
+            regexp = "^[A-Za-zÀ-ÖØ-öø-ÿ\\s'-]{1,}$",
+            message = "First name must be only letters, spaces, apostrophes or hyphens."
+    )
     private String name;
+
     @Pattern(
             regexp = "^[a-z0-9_]{1,12}$",
             message = "Username must be at most 12 characters long, contain only lowercase letters, no spaces and only allowed special characters: _"
     )
     private String username;
+
     @Pattern(
             regexp = "^(?=.*[A-Z])(?=.{12,}$)(?!.*\\s)[A-Za-z\\d!@_-]+$",
             message = "Password must be at least 12 characters long, contain at least one uppercase letter, no spaces, and only allowed special characters: ! @ _ -"
     )
-
     private String password;
+
     private String role;
 }
