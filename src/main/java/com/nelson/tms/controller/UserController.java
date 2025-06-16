3cffaedcc4d0d12f25217842500a6336965d16f4
nelsonsilva-code <nelson.silva@vwds.pt>
2025-06-16 17:03:06 +0100

    :bug: Fix endpoints

--- src/main/java/com/nelson/tms/controller/UserController.java
+++ src/main/java/com/nelson/tms/controller/UserController.java
@@ -30,21 +30,21 @@ public class UserController {
     }
 
     @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER')")
-    @PostMapping("/users/create")
+    @PostMapping("/create")
     public ResponseEntity<String> createUser(@Valid @RequestBody CreateUserDto createUserDto) {
         userService.createUser(createUserDto);
         return ResponseEntity.ok().build();
     }
 
     @PreAuthorize("hasRole('ADMIN')")
-    @DeleteMapping("/users/{id}/delete")
+    @DeleteMapping("/{id}/delete")
     public ResponseEntity<HttpStatus> delete(@PathVariable Long id) {
         HttpStatus httpStatus = userService.delete(id);
         return ResponseEntity.status(httpStatus).build();
     }
 
     @PreAuthorize("hasAnyRole('ADMIN', 'MANAGER','USER')")
-    @PatchMapping("/users/{id}/update-password")
+    @PatchMapping("/{id}/update-password")
     public ResponseEntity<HttpStatus> updatePassword(@Valid @RequestBody UpdatePasswordDto updatePasswordDto, @PathVariable Long id){
         HttpStatus httpStatus = userService.updatePassword(updatePasswordDto, id);
         return ResponseEntity.status(httpStatus).build();
